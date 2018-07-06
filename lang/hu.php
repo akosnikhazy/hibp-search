b16622e36a68fe470ab96290aac402a3e04da9d4
Ákos Nikházy <nikhazy.akos@gmail.com>
2018-07-06 09:28:54 +0200

    Create hu.php

---
+++ lang/hu.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	hu.php - English language file
+	
+	This software created by Ákos Nikházy
+
+*/
+$expressions =
+array(
+'changePassword'		=> 'Change Password',
+'continue'				=> 'Continue',
+'done'					=> 'Done',
+'due'					=> 'Due',
+'errorEmptyPassword'	=> 'Fill all fields',
+'errorSaveReport'		=> 'Error saving report file',
+'errorPasswordNotSame'	=> 'New Password and its repeat are not the same',
+'errorWrongPassword'	=> 'Wrong Password',
+'fileName'				=> 'File',
+'finished'				=> 'Finished',
+'goaway'				=> 'Go Away',
+'htmlTitle'				=> 'Email Tester',
+'htmlTitleReport'		=> 'Report Reader',
+'legend'				=> 'Legend',
+'legendOKText'			=> 'Email address is not in HIBP database',
+'legendNOTOKText'		=> 'Email address is in HIBP database',
+'login'					=> 'Login',
+'logout'				=> 'Logout',
+'main'					=> 'Main',
+'newPW'					=> 'New Password',
+'password'				=> 'Password',
+'pause'					=> 'Pause',
+'repeatPW'				=> 'Repeat',
+'reports'				=> 'Reports',
+'start'					=> 'Start',
+'testedAddress'			=> 'Tested Address',
+'oldPW'					=> 'Current Password'
+);
+?>
