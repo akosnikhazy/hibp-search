f8fcaf0395fde033cb8d8c82a5e05089dff5892c
Ákos Nikházy <nikhazy.akos@gmail.com>
2018-07-06 09:29:13 +0200

    Add files via upload

--- lang/hu.php
+++ lang/hu.php
@@ -1,38 +1,38 @@
 <?php
 /*
-	hu.php - English language file
+	hu.php - Hungarian language file
 	
 	This software created by Ákos Nikházy
 
 */
 $expressions =
 array(
-'changePassword'		=> 'Change Password',
-'continue'				=> 'Continue',
-'done'					=> 'Done',
-'due'					=> 'Due',
-'errorEmptyPassword'	=> 'Fill all fields',
-'errorSaveReport'		=> 'Error saving report file',
-'errorPasswordNotSame'	=> 'New Password and its repeat are not the same',
-'errorWrongPassword'	=> 'Wrong Password',
-'fileName'				=> 'File',
-'finished'				=> 'Finished',
-'goaway'				=> 'Go Away',
-'htmlTitle'				=> 'Email Tester',
-'htmlTitleReport'		=> 'Report Reader',
-'legend'				=> 'Legend',
-'legendOKText'			=> 'Email address is not in HIBP database',
-'legendNOTOKText'		=> 'Email address is in HIBP database',
-'login'					=> 'Login',
-'logout'				=> 'Logout',
-'main'					=> 'Main',
-'newPW'					=> 'New Password',
-'password'				=> 'Password',
-'pause'					=> 'Pause',
-'repeatPW'				=> 'Repeat',
-'reports'				=> 'Reports',
-'start'					=> 'Start',
-'testedAddress'			=> 'Tested Address',
-'oldPW'					=> 'Current Password'
+'changePassword'		=> 'Jelszó váltás',
+'continue'				=> 'Folytatás',
+'done'					=> 'Kész',
+'due'					=> 'Hátralévő',
+'errorEmptyPassword'	=> 'Minden mezőt ki kell tölteni',
+'errorSaveReport'		=> 'Hiba történt a jelentés mentésekor',
+'errorPasswordNotSame'	=> 'A jelszó és ellenőrzése nem egyeznek meg',
+'errorWrongPassword'	=> 'Hibás jelszó',
+'fileName'				=> 'Fájl',
+'finished'				=> 'Eddig tesztelt',
+'goaway'				=> 'Menj innen!',
+'htmlTitle'				=> 'Email Teszter',
+'htmlTitleReport'		=> 'Kimutatások',
+'legend'				=> 'Értelmezés',
+'legendOKText'			=> 'Az Email cím nincs benne a HIBP adatbázisban',
+'legendNOTOKText'		=> 'Az Email cím benne van a HIBP adatbázisban',
+'login'					=> 'Belépés',
+'logout'				=> 'Kijelentkezés',
+'main'					=> 'Főoldal',
+'newPW'					=> 'Új jelszó',
+'password'				=> 'Jelszó',
+'pause'					=> 'Szüneteltetés',
+'repeatPW'				=> 'Újra',
+'reports'				=> 'Eredmények',
+'start'					=> 'Kezdés',
+'testedAddress' 		=> 'Tesztelt cím',
+'oldPW'					=> 'Jelenlegi jelszó'
 );
 ?>
